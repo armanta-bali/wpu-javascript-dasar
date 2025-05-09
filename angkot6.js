d6af7661d116445a2e5f905d6117d2a6a052d42a
Armanta <armanto.rgfamily@gmail.com>
2025-05-09 13:12:27 +0800

    Create angkot6.js
    update for angkot5.js

---
+++ angkot6.js
@@ -0,0 +1,13 @@
+//pengulangan dengan for dan pengkondisian if
+let angkotBeroperasi = 6;
+let jumlahAngkot = 10;
+
+for(let nomorAngkot = 1; nomorAngkot <= jumlahAngkot; nomorAngkot++) {
+  if(nomorAngkot <= angkotBeroperasi && nomorAngkot != 5) {
+    console.log(`Angkot No. ${nomorAngkot} beroperasi dengan baik.`);
+  } else if(nomorAngkot === 8 || nomorAngkot === 10 || nomorAngkot === 5) {
+    console.log(`Angkot No. ${nomorAngkot} sedang lembur.`);
+  } else {
+    console.log(`Angkot No. ${nomorAngkot} sedang rusak.`);
+  }
+}
