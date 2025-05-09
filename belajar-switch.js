db4031318578fa50deed2c122290432eaa24779f
Armanta <armanto.rgfamily@gmail.com>
2025-05-09 13:37:25 +0800

    Update belajar-switch.js
    fix semicolon to colon and typo and add default

--- belajar-switch.js
+++ belajar-switch.js
@@ -1,13 +1,14 @@
-let items = prompt(`Masukan makanan atau minuman \n (contoh: nasi, daging, susu, humberger, softdrink)`);
+let items = prompt(`Masukan makanan atau minuman \n (contoh: nasi, daging, susu, hamburger, softdrink)`);
 
 switch(items) {
-  case "nasi";
-  case "daging";
-  case "susu";
+  case "nasi":
+  case "daging":
+  case "susu":
     alert(`makanan atau minuman sehat`);
     break;
-  case "humberger";
-  case "softdrink";
+  case "humberger":
+  case "softdrink":
     alert(`makanan atau miniman tidak sehat`);
     break;
+  default("Anda memasukan makanan atau minuman yang salah");
 }
